aa3a8f2ab7de02f6b3320389c5691c68f908e9eb
Obyduzzaman-Abir <obyduzzamanabir@gmail.com>
2025-08-09 20:46:41 +0600

    This is 5 subject percentage

---
+++ find-percentage-5-subject.c
@@ -0,0 +1,13 @@
+// Find out 5 subjects percentage
+#include<stdio.h>
+int main()
+{
+    float m1 =100;
+    float m2 = 77;
+    float m3 = 92;
+    float m4 =96;
+    float m5 =86 ;
+    float p = (m1 + m2 + m3 + m4 + m5)/5;
+    printf("Percentage of 5 subjects is : %.2f" , p);
+    return 0;
+}
