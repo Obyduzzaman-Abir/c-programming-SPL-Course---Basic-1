cb0e46d1a0413ce6490ef3c0bb05e32618ace34f
Obyduzzaman-Abir <obyduzzamanabir@gmail.com>
2025-08-10 20:53:44 +0600

    Reminder

---
+++ findthe-reminder.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b; //a>b
+    printf("Enter dividend : ");
+    scanf("%d" ,&a);
+    printf("Enter divisor : ");
+    scanf("%d" ,&b);
+    int q= a/b;
+    int r = a -b*q;
+    printf("your Reminder when %d is divided by %d is %d", a ,b,r);
+    return 0;
+}
